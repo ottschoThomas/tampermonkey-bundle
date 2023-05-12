1b40fb94dddc234b9bbcde12ee841f9369704d96
Tomek L <thomas.lesinski@ottscho.de>
2023-05-12 23:08:15 +0200

    Update

--- Example.user.js
+++ Example.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         GitHub Shopware - Copy extends path
 // @namespace    ott
-// @version      3
+// @version      4
 // @description  Add btn to copy path for template extension
 // @author       Thomas Lesinski
 // @include      https://www.google.de*
 // @grant        none
 // @downloadURL  https://github.com/ottschoThomas/tampermonkey-bundle-2/raw/main/Example.user.js
+// @updateURL    https://github.com/ottschoThomas/tampermonkey-bundle-2/raw/main/Example.user.js
 // ==/UserScript==
 
 (function(){'use strict';
