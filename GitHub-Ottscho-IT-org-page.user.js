103864a4f1a352e1ae824f2cb657d6eadb2ecb7d
Tomek L <thomas.lesinski@ottscho.de>
2023-05-13 18:59:04 +0200

    Update

---
+++ GitHub-Ottscho-IT-org-page.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         GitHub Ottscho IT org page
+// @namespace    ott
+// @version      1
+// @description  Focus repository search input field on page load
+// @author       Thomas Lesinski
+// @match        https://github.com/ottschoIT?view_as=member*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// @downloadURL  https://github.com/ottschoThomas/tampermonkey-bundle/raw/main/GitHub-Ottscho-IT-org-page.js
+// @updateURL    https://github.com/ottschoThomas/tampermonkey-bundle/raw/main/GitHub-Ottscho-IT-org-page.js
+// ==/UserScript==
+!function(){"use strict";document.querySelector("#your-repos-filter").focus()}();
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Tampermonkey Script Bundle
+
+## How to install/update?
+```
+https://github.com/ottschoThomas/tampermonkey-bundle/archive/refs/heads/main.zip
+```
+![How to import](Docs/how_to_import_1.png)
+![How to import](Docs/how_to_import_2.png)
